72b3e5c2f68e225d26b7552a595c75998c9eac6a
Soo <sooyc26@gmail.com>
2018-11-21 17:27:53 -0800

    withAPI

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="">
+        <header className="">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
